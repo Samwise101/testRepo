b9579feee37d0da66c0f2c9726e9637249d01e91
SamuelPalenik111128 <xpaleniks@stuba.sk>
2022-10-01 18:16:49 +0200

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
+#include <memory>
+
+class MyClass{
+public:
+    //constructor
+    MyClass() {
+        std::cout << "Constructor invoked!" << '\n';
+    }
+    //destructor
+    ~MyClass() {
+        std::cout << "Destructor invoked!" << '\n';
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //unique pointer
+    std::unique_ptr<MyClass> unPtr1 = std::make_unique<MyClass>();
+    std::unique_ptr<MyClass> unPtr2 = std::move(unPtr1);
+
+    //shared pointer
+    std::shared_ptr<MyClass> shPtr1 = std::make_shared<MyClass>();
+    std::cout << shPtr1.use_count() << '\n';
+
+    {
+        std::shared_ptr<MyClass>sharedPtr = shPtr1;
+        std::cout << shPtr1.use_count() << '\n';
+    }
+    std::cout << shPtr1.use_count() << '\n';
+
+    std::weak_ptr<int> weakPtr1;
+    {
+        std::shared_ptr<int> shPtr3 = std::make_shared<int>(5);
+        weakPtr1 = shPtr3;
+    }
+
     return 0;
 }
